4b5e622257d8b7781735b2112bf175a39eb13d78
Kristian Cross <etamin1985@yahoo.com>
2026-03-07 18:47:09 +0000

    Add routes for each page

---
+++ src/router.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './views/Home.vue'
+import Projects from './views/Projects.vue'
+import ProjectDetail from './views/ProjectDetail.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    component: Projects
+  },
+  {
+    path: '/projects/:id',
+    name: 'ProjectDetail',
+    component: ProjectDetail,
+    props: true
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
+})
+
+export default router
