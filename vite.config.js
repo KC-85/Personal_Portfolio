1a919ba54030d2ed05d8badaf9cc1f420cf4da81
Kristian Cross <etamin1985@yahoo.com>
2026-03-08 17:41:39 +0000

    Add FastAPI target to viteconfig

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ export default defineConfig(({ command }) => ({
   plugins: [vue()],
   base: command === 'build' ? '/Personal_Portfolio/' : '/',
   root: '.',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true
+      }
+    }
+  },
   build: {
     outDir: 'dist'
   }
